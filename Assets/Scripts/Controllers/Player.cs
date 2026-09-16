fee8abcc8dc9134b3a5c7fe7216c03e608cfc0b3
moham187 <moham187@OA-S141-26>
2026-09-16 10:45:54 -0400

    Exercise 1 - Added spawming bomb

---
+++ Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Keyboard.current.bKey.wasPressedThisFrame) 
+        {
+            SpawnBombAtOffest(Vector3.up);
+        }
+        
+    }
+
+    void SpawnBombAtOffest(Vector3 inOffset)
+    {
+        Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);
+    }
+}
