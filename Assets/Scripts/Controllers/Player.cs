2d98186bf545ca048c7cbdffd40d68e1a39bc8c4
moham187 <moham187@OA-S141-26>
2026-09-16 11:07:47 -0400

    Finished exercise 2 and started a bit of exercise 3

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -11,15 +11,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if(Keyboard.current.bKey.wasPressedThisFrame) 
         {
             SpawnBombAtOffest(Vector3.up);
+            //run output of vector
+            Normalized();
         }
-        
+
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+
+        }
+
+
     }
 
     void SpawnBombAtOffest(Vector3 inOffset)
     {
         Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);
+    }
+
+    void Normalized()
+    {
+        Vector2 playerpos = new Vector2(3, 4);
+        playerpos.Normalize();
+        Debug.Log(playerpos);
+    }
+
+    void Jump()
+    {
+        Vector2 enemypos = enemyTransform.position;
+        //calculating the distance using vector substraction.
+        Vector2 direction = enemyTransform.position - transform.position;
     }
 }
